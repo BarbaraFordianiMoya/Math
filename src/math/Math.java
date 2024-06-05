0a7ae812e493a421ca598db9dcf269d9fb5ddbdb
Aluno <Aluno@10.85.198.10>
2024-06-05 16:27:30 -0300

    Math

---
+++ src/math/Math.java
@@ -0,0 +1,21 @@
+
+package math;
+
+
+public class Math {
+
+    public static void main(String[] args) {
+        int b = 3;
+        int h = 4;
+        
+        double z = b*h*0.5;
+        
+        System.out.println(z);
+        
+        int x = 47;
+        double s = x-45;
+        double n = x/45;
+        System.out.println("Numero de crianças que vão de van: " + s);
+        System.out.println("Numero de onibus que vão precizar: " + n);
+    }   
+}
